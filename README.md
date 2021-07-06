49834941c6d25756d01871b651d6fdb3bf1d8e10
Edoardo <no>
2021-07-06 16:04:13 +0200

    1.0.4

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A simple checksum utility for [Node-RED](https://nodered.org/), based on [crypto
 
 ### Prerequisites
 
-Node-RED installed. Latest version should work.
+Node-RED installed. Latest version should work fine.
 
 
 ### Installation
@@ -28,8 +28,7 @@ If necessary, restart Node-RED.
 
 ### How to use
 
-The node must get as input a msg object, containing the 'payload' and 'checksum' properties.
-If one of these is missing, the output will be a string telling so.
+The node must get as input a *msg* object, containing both the 'payload' (string, or path to the file, to be hashed) and 'checksum' (the checksum as a string) properties.
 
 There are 2 options in the Properties tab of the node:
 - Hash function: allows to choose the necessary function between those supported.
@@ -41,6 +40,11 @@ The node returns a boolean if everything works correctly: true if the checksum g
 ### Examples
 
 As an implementation example, see [this flow](https://flows.nodered.org/flow/33b68d640eac3e9a4a29441285a6f4ea).
+
+
+### Security note
+
+Path traversal is not taken into consideration here, since the node only elaborates the checksum and returns a boolean for result.
 
 
 ### Contribution
--- checksum/checksum.js
+++ checksum/checksum.js
@@ -16,23 +16,37 @@ module.exports = function(RED) {
 
             var HASH = require("crypto-js/" + node.hashFunc);
 
+            var hashInput;
+
             //if payload is a file
             if (node.isFile){
 
                 var fs = require("fs");
 
-                var data = fs.readFileSync(msg.payload).toString();
+                try {
+                    hashInput = fs.readFileSync(msg.payload).toString();
 
-                cs = HASH(data).toString();
+                  } catch (err) {
+                    if (err.code === 'ENOENT'){
+                        node.warn("File not found! Check the path provided.");
+                        return null;
+                    } else{
+                        throw err;
+                    }
+                  }
 
             //if payload is a string
             } else {
 
-                cs = HASH(msg.payload).toString();
+                hashInput = msg.payload;
             }
 
+            //hashing
+            cs = HASH(hashInput).toString();
+
             if (!msg.checksum || !msg.payload){
-                msg.payload = "Missing parameters (payload or checksum)!";
+                node.warn("Missing parameters (payload or checksum)!");
+                return null;
 
             }else{
 
